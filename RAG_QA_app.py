1ab412752851a84f10ea375a0240b58ab80911ed
priyabrata jena <39461119+priya369@users.noreply.github.com>
2025-06-24 18:30:08 +0530

    Update RAG_QA_app.py

--- RAG_QA_app.py
+++ RAG_QA_app.py
@@ -2,7 +2,6 @@ import streamlit as st
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_chroma import Chroma
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.prompts import ChatPromptTemplate,MessagesPlaceholder
@@ -11,7 +10,6 @@ from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
-from chromadb.config import Settings
 from langchain_community.vectorstores import FAISS
 import os
 from dotenv import load_dotenv
